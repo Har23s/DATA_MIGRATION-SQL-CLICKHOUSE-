8a6c19957d7e44c689b4de57cce3c4016f12f0de
Har23s <158301370+Har23s@users.noreply.github.com>
2025-12-12 23:43:04 +0530

    Update Connection.py

--- Connection.py
+++ Connection.py
@@ -16,7 +16,7 @@ sql_connection_properties = {
 # clcikhouse config
 clickhouse_host = " "
 clickhouse_port = " "
-clickhouse_database = ""
+clickhouse_database = " "
 clickhouse_user = ""
 clickhouse_password = ""
 
